8410a5ee9b7c56c1430b734b174af2b936589330
Taha Smaoui <tahasmaoui1993@gmail.com>
2024-01-18 21:54:14 +0100

    Created player.js inside the src folder & added the Json objects containig the players details

---
+++ src/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Kylian Mbappé",
+      team: "Paris Saint-Germain",
+      nationality: "France",
+      jerseyNumber: 7,
+      age: 25,
+      imageUrl: "https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg)/origin-imgresizer.eurosport.com/2022/04/03/3349464-68469548-2560-1440.jpg",
+    },
+    {
+      name: "Erling Haaland",
+      team: "Manchester City",
+      nationality: "Norway",
+      jerseyNumber: 9,
+      age: 23,
+      imageUrl: "https://assets-fr.imgfoot.com/media/cache/1200x1200/erling-haaland-2324-xf.jpg",
+    },
+    {
+        name: "Mohamed Salah",
+        team: "Liverpool",
+        nationality: "Egypt",
+        jerseyNumber: 11,
+        age: 31,
+        imageUrl: "https://ichef.bbci.co.uk/news/976/cpsprodpb/12DB/production/_104172840_gettyimages-923757862.jpg",
+      },
+      {
+        name: "Jude Bellingham",
+        team: "Real Madrid",
+        nationality: "England",
+        jerseyNumber: 5,
+        age: 20,
+        imageUrl: "https://imgresizer.eurosport.com/unsafe/2560x1440/filters:format(jpeg)/origin-imgresizer.eurosport.com/2023/10/09/3800763-77305188-2560-1440.jpg",
+      }
+    
+  ];
+  
+  export default players;
