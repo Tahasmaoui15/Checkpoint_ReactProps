1b859ad14ed3e7eea7cbbab5464a799c91baf440
Taha Smaoui <tahasmaoui1993@gmail.com>
2024-01-19 22:10:25 +0100

    Added an alert that appears when a button within the cards is clicked on.

--- src/Player.js
+++ src/Player.js
@@ -3,6 +3,7 @@ import Card from "react-bootstrap/Card";
 import Button from 'react-bootstrap/Button';
 
 const Player = ({ name, team, nationality, jerseyNumber, age, imageUrl }) => {
+  const handleClick= () => alert (`Information about ${name} will be added soon.`)
   return (
     // Inline styling
     <Card style={{display: "flex", flexWrap: "wrap", width: "18rem", margin: "10px", border: "2px solid", borderRadius: "8px", cursor:"pointer" }}>
@@ -16,7 +17,7 @@ const Player = ({ name, team, nationality, jerseyNumber, age, imageUrl }) => {
           Age: {age}
         </Card.Text>
         {/* Adding a button to the Cards from ReactBootstrap */}
-        <Button> More </Button>
+        <Button onClick={handleClick}> More </Button>
       </Card.Body>
     </Card>
   );
